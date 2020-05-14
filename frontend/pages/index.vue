839d51652f4ab3a068383e954933903601e73d23
Christian Kemper <sikeidev@gmail.com>
2020-05-14 12:00:12 +0200

    Update index.vue

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -3,7 +3,7 @@
     <div>
       <logo />
       <h1 class="title">
-        frontend
+        nice frontend
       </h1>
       <h2 class="subtitle">
         My bedazzling Nuxt.js NEW project # {{  envVar }} #
